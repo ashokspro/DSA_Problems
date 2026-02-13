24d1e4786ca57acd5166afe19f422d024fd34cc3
ashokspro <ashokpcshanmugam@gmail.com>
2026-02-13 20:52:03 +0530

    Defanging an IP Address

---
+++ Problems/PrimeNum.py
@@ -0,0 +1,19 @@
+def prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+"Efficient"
+def prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):  # check only odd numbers
+        if num % i == 0:
+            return False
+    return True
