afa30ccdb3487c9a56819a2557877fa4f7acbd05
ashokspro <ashokpcshanmugam@gmail.com>
2026-03-01 21:17:11 +0530

    4 Sum Modified

--- Striver/4Sum.py
+++ Striver/4Sum.py
@@ -4,12 +4,10 @@ class Solution:
         res = []
 
         for i in range(len(nums) - 3):
-            # skip duplicate starting values
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
             for j in range(i+1, len(nums) - 2):
-                # skip duplicate starting values
                 if j > i + 1 and nums[j] == nums[j-1]:
                     continue
                 left, right = j + 1, len(nums) - 1
