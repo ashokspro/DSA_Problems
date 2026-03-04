1358a975008928e37aa9f13ff697d60e065495df
ashokspro <ashokpcshanmugam@gmail.com>
2026-03-04 21:29:09 +0530

    MinimumTimeVisitingAllPoints

---
+++ Problems/MinimumTimeVisitingAllPoints.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        seconds = 0
+        
+        x1, y1 = points.pop()
+
+        while points:
+            x2, y2 = points.pop()
+            seconds += max(abs(x2 - x1), abs(y2 - y1))
+            x1, y1 = x2, y2
+
+        return seconds
