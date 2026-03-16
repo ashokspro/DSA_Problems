af98e0aae6978d8e9229a61db1ee3e0523982f9f
ashokspro <ashokpcshanmugam@gmail.com>
2026-03-16 15:15:22 +0530

    Merge Intervalas

--- Striver/MergeIntervals.java
+++ Striver/MergeIntervals.java
@@ -1,26 +1,24 @@
 package Striver;
+
 import java.util.*;
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Sort intervals based on start time
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        List<int[]> merged = new ArrayList<>();
-        int[] prev = intervals[0];
-
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
         for (int i = 1; i < intervals.length; i++) {
-            // Overlap condition: current start <= previous end
-            if (intervals[i][0] <= prev[1]) {
-                // Merge intervals
-                prev[1] = Math.max(prev[1], intervals[i][1]);
+            int[] last = res.get(res.size() - 1);
+
+            if (last[1] >= intervals[i][0]) {
+                last[1] = Math.max(intervals[i][1], last[1]);
             } else {
-                // No overlap, add previous interval to result
-                merged.add(prev);
-                prev = intervals[i];
+                res.add(intervals[i]);
             }
         }
 
-        merged.add(prev); // Add the last interval
-        return merged.toArray(new int[merged.size()][]);
+        return res.toArray(new int[res.size()][]);
+
     }
 }
