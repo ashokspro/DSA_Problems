ae2a7c89bf81bf9d3c7576c013e841a26954b9d1
ashokspro <ashokpcshanmugam@gmail.com>
2026-03-23 21:19:59 +0530

    Koko Eating Bananas

--- Striver/Solution.java
+++ Striver/Solution.java
@@ -1,27 +1,38 @@
 public class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
-        int ans = right;
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt(); 
 
-        while (left <= right) {
+        while (left <= right){
+
             int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;     
+            long time_taken = hours(piles, mid);
+
+            if (time_taken <= h){
                 right = mid - 1;
-            } else {
-                left = mid + 1; 
+
+            }else{
+                left = mid + 1;
             }
+
         }
-        return ans;
+
+        return left;
+        
+
     }
 
-    public boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
-        }
-        return hours <= h;
+
+
+    public long hours(int[] piles, int k){
+    long hour = 0;
+
+    for (int i = 0; i < piles.length ; i++){
+        hour += (piles[i] + k - 1) / k;
+
+        if (hour > Integer.MAX_VALUE) return hour; 
     }
+
+    return hour;
+}
 }
