45086245b9499c63a7ba7009851a9983f0033874
ashokspro <ashokpcshanmugam@gmail.com>
2026-04-05 12:23:44 +0530

    Search a 2D Matrix

--- Striver/Searcha2DMatrix.java
+++ Striver/Searcha2DMatrix.java
@@ -1,25 +1,23 @@
-class Solution {
+package Striver;
+
+public class Searcha2DMatrix {
+
+    class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
         int left = 0;
-        int right = rows * cols - 1;
+        int right = m * n - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int row = mid / cols;
-            int col = mid % cols;
-            int guess = matrix[row][col];
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
 
-            if (guess == target) {
-                return true;
-            } else if (guess < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) right = mid - 1;
+            else left = mid + 1;
         }
-
-        return false;        
+        return false;
     }
+}
 }
